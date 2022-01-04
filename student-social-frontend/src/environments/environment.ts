727cba1b656613667c465d5a6c3310d487f20f76
Gal Iulian <iuliangal186@gmail.com>
2022-01-04 21:55:41 +0200

    reset port

--- student-social-frontend/src/environments/environment.ts
+++ student-social-frontend/src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  baseUrl:"http://localhost:8000",
+  baseUrl:"http://localhost:8080",
   registerUrl:"/user/register",
   loginUrl:"/user/login",
   subjectUrl:"/subject",
